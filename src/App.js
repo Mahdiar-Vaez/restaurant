a40206081e54ba194fa06f48841e06052325ac0a
Mahdiar <mahdyarvaez@gmail.com>
2024-05-26 12:38:50 -0700

    making components and pages structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react'
-
+import React from "react";
+import { Routes,Route,Navigate } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import Foods from "./Pages/Foods";
+import FoodDetail from "./Pages/FoodDetail";
+import Cart from "./Pages/Cart";
+import LoginRegister from "./Pages/LoginRegister";
+import Footer from "./Components/Footer";
+import NotFound from "./Pages/NotFound";
 export default function App() {
   return (
-    <div>سلام 
-      من مهدیارم
-     
-    </div>
-  )
+    <>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route  path="/foods" element={<Foods/>} />
+        <Route path="/food-detail/:id/:name" element={<FoodDetail/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/login-register" element={<LoginRegister/>} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+      <Footer/>
+      
+    </>
+  );
 }
