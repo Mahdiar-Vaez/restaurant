e31aa4aff6201edd09031f36e86de3775372900f
Mahdiar <mahdyarvaez@gmail.com>
2024-05-29 11:00:11 -0700

    hamburger section

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SwiperSec from "../../Components/Swiper";
 import "./home.css";
 import pizza from "../../assets/pizza.jpg";
@@ -10,6 +10,29 @@ import bannerBurger from '../../assets/banner-home-burger.jpg'
 import bannerChicken from '../../assets/banner-home-chicken.jpg'
 import { Link } from "react-router-dom";
 export default function Home() {
+  const [hamburger,setHamburger]=useState()
+  useEffect(()=>{
+    (async()=>{
+      try {
+        const res=await fetch("http://localhost:3001/hamburger")
+        const data=await res.json()
+        setHamburger(data)
+
+      } catch (error) {
+        alert ('can not fetch')
+      }
+    })()
+  },[])
+  const hamburgerItems= hamburger?.map((e,index)=>{
+    return <div className="hamburger-items">
+      <img src={e?.img} alt={e?.name} />
+      <span>{e?.price} تومان</span>
+      <h5>{e?.name}</h5>
+      <p>{e?.des}</p>
+      <div> <button>سفارش</button></div>
+     
+    </div>
+  })
   return (
     <>
       <SwiperSec />
@@ -58,8 +81,8 @@ export default function Home() {
           <span>ترد</span>
         </div>
       </div>
-      <div className="hamburgers">
-
+      <div className="hamburgers section">
+      {hamburgerItems}
       </div>
     </>
   );
